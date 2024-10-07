51f98fa79ebb1ca1fc964828a4d250a1a206be9a
Rodrigo Coutrufo Silva <coutruforodrigo@gmail.com>
2024-10-07 19:24:06 -0300

    sale_controller.py

--- app/controllers/sale_controller.py
+++ app/controllers/sale_controller.py
@@ -10,21 +10,21 @@ def create_sale(data):
     product_id = data.get('product_id')
     quantity = data.get('quantity')
 
-    # Verificar se o produto existe
+  
     product = Product.query.get(product_id)
     if not product:
         return jsonify({"message": "Produto não encontrado."}), 404
 
-    # Verificar se há estoque suficiente
+    
     if product.stock < quantity:
         return jsonify({"message": "Estoque insuficiente."}), 400
 
-    # Calcular o preço total da venda
+   
     sale_price = product.price * quantity
     sale = Sale(user_id=user_id, product_id=product_id, quantity=quantity, price=sale_price)
     db.session.add(sale)
 
-    # Atualizar o estoque do produto
+    
     product.stock -= quantity
     db.session.commit()
 
